5fbc13b6460de1a229d58a8028683cac71f468a5
Markus Palme <mpalme@portalworkers.com>
2024-12-11 12:30:13 +0100

    Reproduction

---
+++ AppDelegate.cs
@@ -0,0 +1,33 @@
+using System.Globalization;
+
+namespace stringcontains;
+
+[Register("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate
+{
+    public override UIWindow? Window { get; set; }
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        // create a new window instance based on the screen size
+        Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+        var contains = "".Contains("ö", StringComparison.CurrentCultureIgnoreCase);
+
+        // create a UIViewController with a single UILabel
+        var vc = new UIViewController();
+        vc.View!.AddSubview(new UILabel(Window!.Frame)
+        {
+            BackgroundColor = UIColor.SystemBackground,
+            TextAlignment = UITextAlignment.Center,
+            Text = "Contains result: " + contains + ", current culture: " + CultureInfo.CurrentCulture.Name,
+            AutoresizingMask = UIViewAutoresizing.All,
+        });
+        Window.RootViewController = vc;
+
+        // make the window visible
+        Window.MakeKeyAndVisible();
+
+        return true;
+    }
+}
